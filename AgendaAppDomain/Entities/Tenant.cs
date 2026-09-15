784d678a880fec34e2b733c0b5e7975ec31cd62a
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-14 21:15:11 -0300

    feat: criando entidade Tenant

---
+++ AgendaAppDomain/Entities/Tenant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaApp.Domain.Entities;
+
+public class Tenant
+{
+    public Guid Id { get; private set; }
+    public string Nome { get; private set; }
+    public DateTime DataCriacao { get; private set; }
+    public bool Ativo { get; private set; }
+
+    public Tenant(string nome)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(nome);
+
+        Id = Guid.NewGuid();
+        Nome = nome.Trim();
+        DataCriacao = DateTime.UtcNow;
+        Ativo = true;
+    }
+    private Tenant()
+    { }
+
+    public void Desativar()
+    {
+        Ativo = false;
+    }
+}
