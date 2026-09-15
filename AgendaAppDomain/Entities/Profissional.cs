3d4eb3f295ce31777b7fd6a668ca603f07b5f58c
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-14 22:34:42 -0300

    feat: add Entidade Profissional

---
+++ AgendaAppDomain/Entities/Profissional.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaApp.Domain.Entities;
+
+public class Profissional : Entity
+{
+    public Guid TenantId { get; private set; }
+    public string Nome { get; private set; }
+
+    public Profissional(Guid tenantId, string nome) : base()
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(nome);
+
+        if (tenantId == Guid.Empty)
+        {
+            throw new ArgumentException("A empresa não pode estar vazia", nameof(tenantId));
+        }
+
+        TenantId = tenantId;  
+        Nome = nome.Trim();
+    }
+
+    private Profissional() { }
+}
