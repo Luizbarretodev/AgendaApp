91d718573293977066b1890f366c1c74262732a5
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-14 22:31:58 -0300

    feat:  add classe abstrata base para criação de entidades

---
+++ AgendaAppDomain/Entities/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaApp.Domain.Entities;
+
+public abstract class Entity
+{
+    public Guid Id { get; protected set; }
+    public DateTime DataCriacao { get; protected set; }
+    public bool Ativo { get; protected set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+        DataCriacao = DateTime.UtcNow;
+        Ativo = true;
+    }
+
+    public void Desativar()
+    {
+        Ativo = false;
+    }
+}
